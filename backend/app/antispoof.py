3af4b627a41aa1be183dc531510ba8e355752bb3
KolaAkshitha <137778199+KolaAkshitha@users.noreply.github.com>
2024-11-21 12:48:49 +0530

    temp push

--- backend/app/antispoof.py
+++ backend/app/antispoof.py
@@ -2,15 +2,20 @@ import torch
 from torchvision import transforms
 import librosa
 import numpy as np
+from tensorflow.keras.models import load_model
+
 
 class AntiSpoofingModel(torch.nn.Module):
     def __init__(self, model_path):
         super().__init__()
-        self.model = torch.load(model_path)  # Load pre-trained model
+        self.model = load_model(model_path) # Load pre-trained model
 
-    def predict(self, audio_features):
+    def predict(self, audio_file_path):
         self.model.eval()
-        with torch.no_grad():
-            input_tensor = torch.tensor(audio_features).unsqueeze(0)  # Add batch dimension
-            output = self.model(input_tensor)
-            return torch.sigmoid(output).item()  # Get probability score
+        audio, sr = librosa.load(audio_file_path, res_type='kaiser_fast')
+        mfcc_features = librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=40)
+        mfcc_features_scaled = np.mean(mfcc_features.T, axis=0)
+        mfcc_features_scaled = np.expand_dims(mfcc_features_scaled, axis=0)
+        prediction = self.model.predict(mfcc_features_scaled)
+
+        return prediction
